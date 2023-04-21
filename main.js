3bf2ee2ff025d6aac03013a4ba1a96e3525c0a22
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-04-20 23:08:59 -0400

    Completado codigo piedra papel tijeras

---
+++ main.js
@@ -0,0 +1,57 @@
+// Codigo Papel Tijeras creado por Gabriel Lagrotteria para el curso de Javascript de CoderHouse
+
+let seleccion = prompt("Ingrese Piedra, Papel o Tijeras");
+
+// Defino valores que quiero que recorra como respuesta a mi seleccion
+
+const valorMinimo = 0;
+const valorMaximo = 2;
+
+// Inicia Loop que actua por en base al prompt
+
+while (seleccion !== "esc") {
+    const respuesta = Math.floor(Math.random() * (valorMaximo - valorMinimo + 1)) + valorMinimo;
+    switch (seleccion.toLowerCase()) {
+        case "piedra":
+            if (respuesta === 0) {
+                alert("El oponente escogio Piedra, es un Empate");
+            } else if (respuesta === 1) {
+                alert("El oponente escogio Papel, Gana el Oponente");
+            } else {
+                alert("El oponente escogio Tijeras, Ganaste");
+            }
+            break;
+        case "papel":
+            if (respuesta === 0) {
+                alert("El oponente escogio Piedra,Ganaste");
+            } else if (respuesta === 1) {
+                alert("El oponente escogio Papel, Empate");
+            } else {
+                alert("El oponente escogio Tijeras, Gana el Oponente");
+            }
+            break;
+        case "tijeras":
+            if (respuesta === 0) {
+                alert("El oponente escogio Piedra, Gana el Oponente");
+            } else if (respuesta === 1) {
+                alert("El oponente escogio Papel, Ganaste");
+            } else {
+                alert("El oponente escogio Tijeras, Empate");
+            }
+            break;
+        default:
+            alert("No ha habido seleccion");
+            break;
+    }
+
+// Se selecciona si quieres seguir jugando para asi poder romper el While
+
+    let repetir = prompt("¿Quieres jugar de nuevo? ¿Si o No?").toLowerCase();
+        if(repetir === "no"){
+            break;
+        }else if (repetir !== "si"){
+            alert("Eso es una respuesta invalida, por favor selecciona Si o No");
+            repetir = prompt("¿Quieres jugar de nuevo? ¿Si o No?");
+        } 
+        seleccion = prompt("Ingrese Piedra, Papel o Tijeras");
+}
